48880545d5be615aba5c9eb3f3ac87b350ff9fd0
SayaliShukla25 <104975846+SayaliShukla25@users.noreply.github.com>
2022-05-06 09:57:29 +0530

    Add files via upload

---
+++ Arr12.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace AccessModifier.MyCollection
+{
+    class Arr12
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine(al.Count);
+            Console.WriteLine(al.Capacity);
+            
+            al.Add("sayali");
+            al.Add(20);
+            al.Add("Pune");
+            al.Add("sayalishukla@gmail.com");
+            al.Add(true);
+            al.Remove("sayali");
+            al.RemoveAt(2);
+            al.Insert(1,"shukla");
+            al.Reverse();
+            
+            for(int i=0;i<al.Count;i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            foreach(var i in al)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ Hash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace AccessModifier.MyCollection
+{
+    class Hash
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("Sayali", 80);
+            ht.Add("Shukla", 80);
+            ht.Add("sayalishukla@gmail.com", "student");
+
+            foreach (var k in ht.Keys)
+                Console.WriteLine("Key=" + k + "value " + ht[k]);
+
+
+
+
+        }
+    }
+}
+
